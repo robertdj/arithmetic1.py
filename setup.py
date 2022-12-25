7790f8b1620c969c7796c90217b2f583f2c8fd32
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2022-12-25 19:37:17 +0100

    ✨ Module for simple arithmetic

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="arithmetic1",
+    version="0.0.1",
+    description="Simple arithmetic",
+    author="Robert Dahl Jacobsen",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    license='MIT',
+    tests_require=['pytest'],
+    python_requires=">=3.7, <4"
+)
+
---
+++ src/arithmetic1/simple.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    """
+    Return the sum of two numbers
+    """
+
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Return the difference of two numbers
+
+    """
+
+    return x - y
+
+
